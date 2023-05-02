31ba09b70dbbfdc6dd28426069796aab7f75ce63
adi_walture <adityawalture9298@gmail.com>
2023-05-03 00:51:48 +0530

    added drawer

---
+++ lib/pages/aboutPg.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+  const About({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 209, 255, 207),
+      appBar: AppBar(),
+    );
+  }
+}
