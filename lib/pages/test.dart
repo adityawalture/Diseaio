aec114689a40bfbd0f8f6b0584cba2a63f23d502
adi_walture <adityawalture9298@gmail.com>
2023-05-05 22:17:05 +0530

    merged to main

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-import 'dart:io';
-=======
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:io';
-import 'dart:ui';
->>>>>>> master
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
-<<<<<<< HEAD
-import 'package:image_picker/image_picker.dart';
-=======
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:transparent_image/transparent_image.dart';
->>>>>>> master
+//import 'package:transparent_image/transparent_image.dart';
 
-import '../main.dart';
 
 class TestInput extends StatefulWidget {
   const TestInput({super.key});
@@ -31,11 +20,7 @@ class _TestInputState extends State<TestInput> {
   CameraController? cameraController;
   String output = '';
   String outputs = '';
-<<<<<<< HEAD
-  File? image;
-=======
   File? img;
->>>>>>> master
   late bool loading = true;
 
   @override
@@ -46,16 +31,6 @@ class _TestInputState extends State<TestInput> {
   }
 
   pickImage() async {
-<<<<<<< HEAD
-    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image == null) return null;
-
-    final imageTemp = File(image.path);
-
-    setState(() {
-      this.image = imageTemp;
-      runModel();
-=======
     final img = await ImagePicker().pickImage(source: ImageSource.gallery);
     if (img == null) return null;
 
@@ -71,42 +46,13 @@ class _TestInputState extends State<TestInput> {
     setState(() {
       img = null;
       outputs = '';
->>>>>>> master
     });
   }
 
-  void clearImage() {
-    setState(() {
-      image = null;
-      outputs = '';
-    });
-  }
-
-  // loadCamera() {
-  //   cameraController = CameraController(camera![0], ResolutionPreset.medium);
-  //   cameraController!.initialize().then((value) {
-  //     if (!mounted) {
-  //       return;
-  //     } else {
-  //       setState(() {
-  //         cameraController!.startImageStream((imageStream) {
-  //           cameraImage = imageStream;
-  //           runModel();
-  //         });
-  //       });
-  //     }
-  //   });
-  // }
   runModel() async {
-<<<<<<< HEAD
-    if (image != null) {
-      var predict = await Tflite.runModelOnImage(
-          path: image!.path,
-=======
     if (img != null) {
       var predict = await Tflite.runModelOnImage(
           path: img!.path,
->>>>>>> master
           imageMean: 0.0, // defaults to 117.0
           imageStd: 255.0, // defaults to 1.0
           numResults: 2, // defaults to 5
@@ -121,26 +67,6 @@ class _TestInputState extends State<TestInput> {
       });
     }
   }
-
-  // runModel() async {
-  //   if (cameraImage != null) {
-  //     var predictions = await Tflite.runModelOnImage(
-  //         ),
-  //         // imageHeight: cameraImage!.height,
-  //         // imageWidth: cameraImage!.width,
-  //         imageMean: 127.5,
-  //         imageStd: 127.5,
-  //         //rotation: 90,
-  //         numResults: 2,
-  //         threshold: 0.1,
-  //         asynch: true);
-  //     predictions!.forEach((element) {
-  //       setState(() {
-  //         output = element['label'];
-  //       });
-  //     });
-  //   }
-  // }
 
   loadmodel() async {
     await Tflite.loadModel(
@@ -158,26 +84,6 @@ class _TestInputState extends State<TestInput> {
           Padding(
             padding: EdgeInsets.only(left: 70, right: 70),
             child: Container(
-<<<<<<< HEAD
-              height: 120,
-              width: 120,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-              ),
-              child: image != null
-                  ? Image.file(
-                      image!,
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                    )
-                  : Container(
-                      height: 120,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade300,
-                      ),
-                    ),
-=======
               height: 250,
               width: 250,
               decoration: BoxDecoration(
@@ -188,23 +94,11 @@ class _TestInputState extends State<TestInput> {
                       ? DecorationImage(
                           image: FileImage(img!), fit: BoxFit.cover)
                       : null),
->>>>>>> master
             ),
           ),
           SizedBox(height: 15),
           Text(
             outputs,
-<<<<<<< HEAD
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          ElevatedButton(
-            onPressed: pickImage,
-            child: Text("Pick Image"),
-          ),
-          ElevatedButton(
-            onPressed: clearImage,
-            child: Text("Clear Image"),
-=======
             style:
                 GoogleFonts.roboto(fontSize: 25, fontWeight: FontWeight.w700),
           ),
@@ -224,7 +118,6 @@ class _TestInputState extends State<TestInput> {
               style:
                   GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
             ),
->>>>>>> master
           )
         ],
       ),
@@ -235,15 +128,4 @@ class _TestInputState extends State<TestInput> {
 
 
 
-<<<<<<< HEAD
-// height: MediaQuery.of(context).size.height * 0.7,
-//               width: MediaQuery.of(context).size.width * 0.7,
-//               child: !cameraController!.value.isInitialized
-//                   ? Container()
-//                   : AspectRatio(
-//                       aspectRatio: cameraController!.value.aspectRatio,
-//                       child: CameraPreview(cameraController!),
-//                     ),
-=======
 // height: MediaQuery.of(context).
->>>>>>> master
